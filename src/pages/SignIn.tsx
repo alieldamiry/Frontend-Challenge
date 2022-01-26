d88f1d3bfbf7c66bfdbca37940d5c5f4a682a235
Ali Eldamiry <alieldamiry@gmail.com>
2022-01-26 11:41:34 -0800

    fixed typo

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import FormikControl from "src/components/formik/FromikControl";
+import FormikControl from "src/components/formik/FormikControl";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
